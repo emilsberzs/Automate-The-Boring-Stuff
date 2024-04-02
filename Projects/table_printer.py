1b9cb1cf3cc6ecb065b326c07414016cc889e812
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2024-04-02 12:06:50 +0100

    Nested list tablifier

--- Projects/table_printer.py
+++ Projects/table_printer.py
@@ -3,6 +3,7 @@
 table_data = [['apples', 'oranges', 'cherries', 'bananas'],
               ['Alice', 'Bob','Carol','David'],
               ['dogs','cats','moose','goose']]
+column_widths = []
 
 def print_table(table):
     for list in table:
@@ -10,13 +11,27 @@ def print_table(table):
         #Find the longest word in each list, add 1 (to accomodate spacing between columns), and save it as width of column:
         column_width = 0
         for item in list:
-            if len(item) + 1 > column_width:
-                column_width = len(item) + 1
+            if len(item) + 2 > column_width:
+                column_width = len(item) + 2
         
-        list.append(column_width)
+        column_widths.append(column_width)
 
         #Stuck here, need to format rows and print out. ATBS page 154.
     
+
+    for i in range(0,len(table[0])): #For each row, corresponding to number of elements in each inner list
+        row = ''                     #Initialise row as an empty string
+        for j in range(0,len(table)): #For each inner list
+            row += table[j][i].rjust(column_widths[j]) #Add 
+        print(row)
             
 
-print_table(table_data)
+#Better code, taken from Stack Over Flow
+def print_table_2(tab):
+    for j in range(len(tab[0])):
+        for i in range(len(tab)):
+            m = max([len(s) for s in tab[i]])
+            print(tab[i][j].rjust(m), end=' ')
+        print('')
+
+print_table_2(table_data)
