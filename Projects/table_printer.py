ef951e4f5bcf5677f6ccedb3ab74fdc92f0cdbfd
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2024-04-04 08:17:23 +0100

    Small program to tabularise list and format them neatly

--- Projects/table_printer.py
+++ Projects/table_printer.py
@@ -6,18 +6,13 @@ table_data = [['apples', 'oranges', 'cherries', 'bananas'],
 column_widths = []
 
 def print_table(table):
-    for list in table:
-
+    for list in table
         #Find the longest word in each list, add 1 (to accomodate spacing between columns), and save it as width of column:
         column_width = 0
         for item in list:
             if len(item) + 2 > column_width:
                 column_width = len(item) + 2
-        
         column_widths.append(column_width)
-
-        #Stuck here, need to format rows and print out. ATBS page 154.
-    
 
     for i in range(0,len(table[0])): #For each row, corresponding to number of elements in each inner list
         row = ''                     #Initialise row as an empty string
@@ -25,7 +20,6 @@ def print_table(table):
             row += table[j][i].rjust(column_widths[j]) #Add 
         print(row)
             
-
 #Better code, taken from Stack Over Flow
 def print_table_2(tab):
     for j in range(len(tab[0])):
